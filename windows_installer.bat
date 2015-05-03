2ecae9c6d09dba60723db9f990366e850ca851a4
Denis Merigoux <denis.merigoux@gmail.com>
2015-05-03 17:12:17 +0200

    Rewriting the package into a Documented LaTeX file .dtx. Added custom installer for windows that compiles the .dtx file with LaTeX and export the resulting files into the right directory of the TeX tree.

---
+++ windows_installer.bat
@@ -0,0 +1,39 @@
+@echo off
+pdflatex polytechnique.dtx
+makeindex -s gind.ist -o polytechnique.ind polytechnique.idx
+pdflatex polytechnique.dtx
+del *.aux
+del *.glo
+del *.idx
+del *.ilg
+del *.ind
+del *.log
+del *.toc
+pause
+kpsewhich article.cls> temp.txt
+set /p installdir=<temp.txt
+set installdir=%installdir:~0,-16%
+del temp.txt
+md "%installdir%polytechnique"
+md "%installdir%polytechnique/ressources"
+copy polytechnique.dtx "%installdir%polytechnique"
+copy polytechnique.sty "%installdir%polytechnique"
+md "%installdir:~0,-10%doc/polytechnique"
+copy polytechnique.pdf "%installdir:~0,-10%doc/polytechnique"
+del polytechnique.sty
+cd ressources
+copy *.eps "%installdir%polytechnique/ressources"
+copy *.pdf "%installdir%polytechnique/ressources"
+echo ************************************************
+echo * La base de donnees des packages est en train *
+echo * d'etre mise a jour. Cela peut prendre un peu *
+echo * de temps. Merci de patienter.                *
+echo ************************************************
+initexmf --update-fndb
+echo ************************************************
+echo * Le package a ete installe ! La documentation *
+echo * se trouve a l'emplacement suivant :          *
+echo %installdir:~0,-10%doc/polytechnique
+echo * Bonne utilisation !                          *
+echo ************************************************
+pause
