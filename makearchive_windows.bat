13eee8d05cc6afee82713ed87a0201313a6c252b
Denis Merigoux <denis.merigoux@gmail.com>
2015-05-28 11:08:41 +0200

    Minor fixes Added archiving script

---
+++ makearchive_windows.bat
@@ -0,0 +1,3 @@
+@echo off
+7z a -tzip polytechnique-LaTeX.zip makefile_windows.bat makefile_unix.sh README.md source/*.pdf source/*.eps source/polytechnique.sty source/polytechnique.dtx guide/installation.pdf guide/installation.tex guide/logo.pdf
+pause
